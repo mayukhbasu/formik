801e7d34b8d25c50f53519d1b9ea2a6dd72885e0
Bubul <bubul@Bubuls-MacBook-Pro.local>
2023-08-24 12:30:41 +0530

    displaying error messages

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -1,4 +1,4 @@
-import { useFormik } from 'formik';
+import { Formik, useFormik } from 'formik';
 import React from 'react';
 
 
@@ -33,19 +33,36 @@ const YoutubeForm = () => {
     validate
   });
   
-  
+  console.log('Form errors ', formik.errors )
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
-        <label htmlFor='name'>Name</label>
-        <input type='text' id='name' name='name' onChange={formik.handleChange} 
-        value={formik.values.name}/>
-        <label htmlFor='email'>Email</label>
-        <input type='text' id='email' name='email' value={formik.values.email} 
-        onChange={formik.handleChange}/>
-        <label htmlFor='channel'>Channel</label>
-        <input type='text' id='channel' name='channel' value={formik.values.channel}
-        onChange={formik.handleChange}/>
+        <div className='form-control'>
+          <label htmlFor='name'>Name</label>
+          <input type='text' id='name' name='name' onChange={formik.handleChange}
+          value={formik.values.name}/>
+          {
+            formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null
+          }
+        </div>
+        
+        <div className='form-control'>
+          <label htmlFor='email'>Email</label>
+          <input type='text' id='email' name='email' value={formik.values.email} 
+          onChange={formik.handleChange}/>
+          {
+            formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null
+          }
+        </div>
+        <div className='form-control'>
+          <label htmlFor='channel'>Channel</label>
+          <input type='text' id='channel' name='channel' value={formik.values.channel}
+          onChange={formik.handleChange}/>
+          {
+            formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null
+          }
+        </div>
+        
         <button type='submit'>Submit</button>
       </form>
     </div>
