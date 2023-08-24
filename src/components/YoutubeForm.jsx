0a92967eb57b277e25196314373f0ec3b06bce7a
Bubul <bubul@Bubuls-MacBook-Pro.local>
2023-08-24 11:45:50 +0530

    managed form state using formik

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -3,16 +3,26 @@ import React from 'react';
 
 
 const YoutubeForm = () => {
-  const formik = useFormik({})
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      channel: ''
+    }
+  });
+  console.log(formik.values);
   return (
     <div>
       <form>
         <label htmlFor='name'>Name</label>
-        <input type='text' id='name' name='name'/>
+        <input type='text' id='name' name='name' onChange={formik.handleChange} 
+        value={formik.values.name}/>
         <label htmlFor='email'>Email</label>
-        <input type='text' id='email' name='email'/>
+        <input type='text' id='email' name='email' value={formik.values.email} 
+        onChange={formik.handleChange}/>
         <label htmlFor='channel'>Channel</label>
-        <input type='text' id='channel' name='channel'/>
+        <input type='text' id='channel' name='channel' value={formik.values.channel}
+        onChange={formik.handleChange}/>
         <button>Submit</button>
       </form>
     </div>
