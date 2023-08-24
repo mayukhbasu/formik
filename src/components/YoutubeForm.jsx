37a6fe34342be4f66e7a95573d39722b75dc9374
Bubul <bubul@Bubuls-MacBook-Pro.local>
2023-08-24 12:58:05 +0530

    imporving ux with formik touched

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -33,33 +33,34 @@ const YoutubeForm = () => {
     validate
   });
   
-  console.log('Form errors ', formik.errors )
+  console.log('Form errors ', formik.errors );
+  console.log('Form visited ', formik.touched)
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
         <div className='form-control'>
           <label htmlFor='name'>Name</label>
           <input type='text' id='name' name='name' onChange={formik.handleChange}
-          value={formik.values.name}/>
+          value={formik.values.name} onBlur={formik.handleBlur}/>
           {
-            formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null
+            formik.errors.name && formik.touched.name ? <div className='error'>{formik.errors.name}</div> : null
           }
         </div>
         
         <div className='form-control'>
           <label htmlFor='email'>Email</label>
           <input type='text' id='email' name='email' value={formik.values.email} 
-          onChange={formik.handleChange}/>
+          onChange={formik.handleChange} onBlur={formik.handleBlur}/>
           {
-            formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null
+            formik.touched.email && formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null
           }
         </div>
         <div className='form-control'>
           <label htmlFor='channel'>Channel</label>
           <input type='text' id='channel' name='channel' value={formik.values.channel}
-          onChange={formik.handleChange}/>
+          onChange={formik.handleChange} onBlur={formik.handleBlur}/>
           {
-            formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null
+            formik.touched.channel && formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null
           }
         </div>
         
