4b409e774b74d3f37f38144a31d4d5ec9ac9655d
Bubul <bubul@Bubuls-MacBook-Pro.local>
2023-08-24 12:18:26 +0530

    validation function

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -3,17 +3,40 @@ import React from 'react';
 
 
 const YoutubeForm = () => {
+  const initialValues = {
+    name: '',
+    email: '',
+    channel: ''
+  };
+  const onSubmit = values => {
+    console.log(values);
+  }
+  const validate = values => {
+    let errors = {};
+      let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+      if(!values.name) {
+        errors.name = 'Required!'
+      }
+      if(!values.email) {
+        errors.email = 'Required!'
+      } else if(!regex.test(values.email)) {
+        errors.email = 'Invalid email format'
+      }
+      if(!values.channel) {
+        errors.channel = 'Required!'
+      }
+      return errors;
+  }
   const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      channel: ''
-    }
+    initialValues,
+    onSubmit,
+    validate
   });
-  console.log(formik.values);
+  
+  
   return (
     <div>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <label htmlFor='name'>Name</label>
         <input type='text' id='name' name='name' onChange={formik.handleChange} 
         value={formik.values.name}/>
@@ -23,7 +46,7 @@ const YoutubeForm = () => {
         <label htmlFor='channel'>Channel</label>
         <input type='text' id='channel' name='channel' value={formik.values.channel}
         onChange={formik.handleChange}/>
-        <button>Submit</button>
+        <button type='submit'>Submit</button>
       </form>
     </div>
   );
